a08ec2f49fd664dffe7bf1b5ea549ae1cf58bebf
lilinxi <376181868@qq.com>
2020-11-24 20:08:28 +0800

    daily add at 2020-11-24 20:08:28

--- src/main/java/core/Examples.java
+++ src/main/java/core/Examples.java
@@ -1,21 +1,23 @@
 package core;
 
 public class Examples {
-    public static String QuickSortExample = "function sort() {\n" +
-            "\tif ( 1<2 ) { }\n" +
-            "\telse { \n" +
-            "\t\tin | [head|tail]\n" +
-            "\t\ttail | x\n" +
-            "\t\tif ( x < head ) {\n" +
-            "\t\t\tx | a\n" +
-            "\t\t} else {\n" +
-            "\t\t\tx | b\n" +
-            "\t\t}\n" +
-            "\t\ta|sort|a\n" +
-            "\t\tb|sort|b\n" +
-            " \t\t[a, head, b] | out\n" +
-            "\t}\n" +
-            " }\n" +
-            "\n" +
-            "[5, 6, 3, 2, 7, 8] | sort | stdout";
+    public static String QuickSortExample = """
+            function sort() {
+            	if ( 1<2 ) { }
+            	else { 
+            		in | [head;tail]
+            		tail | x
+            		if ( x < head ) {
+            			x | a
+            		} else {
+            			x | b
+            		}
+            		a|sort()|a
+            		b|sort()|b
+             		[a, head, b] | out
+            	}
+            }
+            [5, 6, 3, 2, 7, 8] | sort() | stdout
+            """;
+
 }
